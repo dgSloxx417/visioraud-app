7664dc7b5953961e0afebf31cd2d0f747b5d57da
Gerald Donkor <donkorgerald8@gmail.com>
2025-10-06 20:39:54 +0000

    initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/Header"
+
+const Page = () => {
+  return (
+    <main className="wrapper page">
+      <Header title="All Videos" subHeader="Public Library" />
+      <h1 className="text-2xl font-karla">Welcome to Visioraud</h1>
+    </main>
+  )
+}
+
+export default Page
